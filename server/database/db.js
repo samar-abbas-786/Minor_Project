bdf3c8cc00dc46036f79cdb77dab970930cfcc64
Samar Abbas <samarabbas172003@gmail.com>
2024-11-11 16:45:14 +0530

    Course processing1

--- server/database/db.js
+++ server/database/db.js
@@ -10,7 +10,7 @@ const dbConnection = async () => {
     console.log("Connected to MongoDB successfully!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -5,6 +5,5 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/addCourses", AddCourses);
-// router.get("/getInstruction", getInstruction);
 
 module.exports = router;
