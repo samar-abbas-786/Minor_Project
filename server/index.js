a22d891804a05ea4514488d9041190cbccde1cf9
Samar Abbas <samarabbas172003@gmail.com>
2025-06-05 18:40:35 +0530

    allow cors origin

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(
   cors({
     methods: ["GET", "PUT", "POST", "DELETE"],
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
